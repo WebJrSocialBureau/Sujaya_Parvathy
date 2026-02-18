7f9932de4dce4d14b63cd719cbfd36f90cdda684
WebJrSocialBureau <webjr.socialbureau@gmail.com>
2026-02-18 12:06:23 +0530

    Corrected the login issue

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     setError("");
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/auth/signup`,
+        `${import.meta.env.VITE_API_URL}/auth/login`,
         { email, password },
       );
       localStorage.setItem("token", res.data.token);
